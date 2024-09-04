0be4ce3446600e44114ab8c7238d9c3ca8eee3ff
LGhiglionee <lghiglione@uade.edu.ar>
2024-09-04 15:21:18 -0300

    Nuevo diseno para mostrar las matrices

--- Diseno.py
+++ Diseno.py
@@ -1,3 +1,24 @@
-def mostrarmatriz_ordenada(matrizalumnos):
+def menu():
+    print('1. Agregar alumno')
+    print('2. Leer alumno')
+    print('3. Actualizar alumno')
+    print('4. Eliminar alumno')
+    print('5. Ver notas')
+    print('6. Agregar nota')
+    print('7. Actualizar nota')
+    print('8. Salir')
+
+def mostrarmatrizalumnos_ordenada(matrizalumnos):
     for fila in matrizalumnos:
-        print(fila)
+        cad = ' | '.join(f'{elemento:<25}' for elemento in fila)
+        print(cad)
+
+def mostrarmatriznotas_ordenada(matriznotas):
+    for fila in matriznotas:
+        cad = ' | '.join(f'{elemento:<25}' for elemento in fila)
+        print(cad)
+
+def mostrarmatrizmaterias_ordenada(matrizmaterias):
+    for fila in matrizmaterias:
+        cad = ' | '.join(f'{elemento:<25}' for elemento in fila)
+        print(cad)
