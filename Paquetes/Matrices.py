74ac523325c9d6f0468e3f280eef034c0fd69c85
LGhiglionee <lghiglione@uade.edu.ar>
2024-10-09 01:08:57 -0300

    Arregle los diccionarios y matrices. Materias y alumnos son diccionarios, y desp una matriz que los conecta

--- Paquetes/Matrices.py
+++ Paquetes/Matrices.py
@@ -1,99 +1,57 @@
 import re, collections, random
 
 def creardicc_alumnos(x):
-    diccalumnos = []
-    while x>=1:
-        legajo = random.randint(111111111,999999999)
-        
+    diccalumnos = {}
+    while x >= 1:
+        legajo = random.randint(111111111, 999999999)
         nombre = random.choice(['Lucas', 'Maximo', 'Franco', 'Felipe', 'Noah', 'Juan', 'Guillermo', 'Pablo', 'Agustin'])
-        apellido = random.choice(['Ghiglione','Bramuglia','Collura','Remonteo', 'Gomez', 'Huchan', 'Fernandez', 'Gonzalez', 'Perez', 'Garcia'])
-        diccalumnos.append (
-        {
-            'Legajos' : legajo,
-            'Nombres' : nombre,
-            'Apellidos' : apellido
-        }
-        )
+        apellido = random.choice(['Ghiglione', 'Bramuglia', 'Collura', 'Remonteo', 'Gomez', 'Huchan', 'Fernandez', 'Gonzalez', 'Perez', 'Garcia'])
+        diccalumnos[legajo] = [nombre, apellido]
         x -= 1
-        diccalumnos.sort(key = lambda x: (int(x["Legajos"])))# el x para que se ordene de manera ascendente
-        diccalumnos.reverse()# y esto lo pasa a descendente
-    
     return diccalumnos
+    '''diccalumnos.sort(key = lambda x: (int(x[legajo])))# el x para que se ordene de manera ascendente
+    diccalumnos.reverse()# y esto lo pasa a descendente'''
 #funciona
 
 def creardicc_materias():
     turnos = ('Mañana', 'Tarde', 'Noche')
-    materias = ('Programación I', 'Fundamentos de Quimica', 'Sistemas de Representación', 'Matematica Discreta', 'Algebra', 'Arquitectura de computadores')
-    dicc_final = []
-    CodigosNoUsados = set(range(111, 999))
-    cont = collections.defaultdict(lambda: collections.defaultdict(int))# crea dicc ==> {'':{'':0}}
-    
-    while CodigosNoUsados:# tuve que hacer un bucle en base a los elementod de codigos no usados, no me cierra
-        for turno in turnos:
-            materias_disponibles = list(materias)
-            
-            for _ in range(len(materias)*3):  # materias * turnos
-                limite = [m for m in materias if cont[turno][m] < 3]
-                
-                if not materias_disponibles or not CodigosNoUsados:
-                    return dicc_final
-                
-                codigo = random.choice(list(CodigosNoUsados))
-                CodigosNoUsados.remove(codigo)
-                
-                materia = random.choice(materias_disponibles)
-                
-                if cont[turno][materia] < 3:
-                    cont[turno][materia] += 1
-                    dicc_final.append  ({
-                        'Codigo': codigo,
-                        'Turno': turno,
-                        'Materia': materia
-                    })
-                    
-    
-    return dicc_final
+    materias_con_codigos = {
+        3180: 'Programación I',
+        91218: 'Fundamentos de Química',
+        31784: 'Sistemas de Representación',          #Le puse codigo manualmente, mas facil
+        31283: 'Matemática Discreta',
+        553123: 'Álgebra',
+        11163: 'Arquitectura de Computadores'
+    }
+    dicc_materias = {}
+    cont = collections.defaultdict(lambda: collections.defaultdict(int))
+
+    for turno in turnos:
+        for codigo, materia in materias_con_codigos.items():
+            if cont[turno][materia] < 3:  # Limitar a 3 asignaciones por turno y materia
+                cont[turno][materia] += 1
+                dicc_materias[codigo] = [turno, materia]
+
+    return dicc_materias
 #ahí lo cambie según el profesor    
         
 
 
-def creardicc_notas(x):#matriz cambiar nombre
-    diccio_notas = [] #matriz cambiar nombre
-    for l in x[1:]:
-        
-        for i in range(len(x)):
-            
-            parcial1 = random.randint(1,10)
-            parcial2 = random.randint(1,10)
-            
-            if parcial1 >= 8 and parcial2 >= 8:
-                final = 'Promocion'
-            elif parcial1 <4 and parcial2 <4:
-                final = 'Recursa'
-            else:
-                final = random.randint(1,10)
-            diccio_notas.append(
-                {
-                    'Parcial 1' : parcial1,
-                    'Parcial 2' : parcial2,
-                    'Final' : final
-                }
-            )
-    return diccio_notas
 
-def combinado(diccalumnos, matrizmateria, matriznotas):
+def combinado(diccalumnos, dicc_final):
+
+    min_materias, max_materias = 0 , 5
     matriz_combinada = []
+    matriz_combinada.append(['Legajo', 'Código Materia', 'Parcial 1', 'Parcial 2', 'Final'])
+    for legajo in diccalumnos.keys():
+        num_materias = random.randint(min_materias, max_materias)  # pone un numero aleatorio para no todos los alumnos esten en todas las materias
+        materias_asignadas = []
+        materias_disponibles = list(dicc_final.keys()) #crea una lista con todos los codigos
+        while len(materias_asignadas) < num_materias and materias_disponibles:
+            codigo = random.choice(materias_disponibles)
+            materias_asignadas.append(codigo)  # pone materia a alumno
+            materias_disponibles.remove(codigo)  # la saca de la otra lsta, para que no hayan repetidas
+        for codigo in materias_asignadas:
+            matriz_combinada.append([legajo, codigo, '-', '-', '-']) #hace la linea de la matriz
 
-    matriz_combinada.append(['Legajo', 'Código Materia', 'Materia', 'Turno', 'Parcial 1', 'Parcial 2', 'Final'])
-
-    for alumno, materia, nota in zip(diccalumnos, matrizmateria, matriznotas):
-        matriz_combinada.append (fila = [
-            alumno['Legajos'],
-            materia['Codigo'], #codigo de materia
-            nota['Parcial 1'],
-            nota['Parcial 2'],
-            nota['Final']
-        ])
-        
-    
     return matriz_combinada
