b651df4103eacfb40bb8e1cc649c442f9f2f44e7
LGhiglionee <lghiglione@uade.edu.ar>
2024-10-11 13:15:21 -0300

    Randomice fecha nacimiento y mail

--- Paquetes/Matrices.py
+++ Paquetes/Matrices.py
@@ -6,7 +6,12 @@ def creardicc_alumnos(x):
         legajo = random.randint(111111111, 999999999)
         nombre = random.choice(['Lucas', 'Maximo', 'Franco', 'Felipe', 'Noah', 'Juan', 'Guillermo', 'Pablo', 'Agustin'])
         apellido = random.choice(['Ghiglione', 'Bramuglia', 'Collura', 'Remonteo', 'Gomez', 'Huchan', 'Fernandez', 'Gonzalez', 'Perez', 'Garcia'])
-        diccalumnos[legajo] = [nombre, apellido]
+        mail = ''.join([nombre[0].lower(), apellido.lower(), '@gmail.com'])
+        dia= random.randint(1,31) #elije un dia posible
+        mes= random.randint(1,12) #meses posibles
+        anio= random.randint(1960, 2007)
+        fecha_nacimiento = '{:02d}/{:02d}/{}'.format(dia,mes,anio)
+        diccalumnos[legajo] = [nombre, apellido, mail, fecha_nacimiento]
         x -= 1
     return diccalumnos
 
