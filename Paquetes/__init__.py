4a484d34faf0ec26e16d41c38e8ccf6a7151ed89
LGhiglionee <lghiglione@uade.edu.ar>
2024-10-12 20:04:22 -0300

    Funcion asignar materia

--- Paquetes/__init__.py
+++ Paquetes/__init__.py
@@ -8,7 +8,7 @@ from .Diseno import *
 __all__ = [
     #funciones de aritmetica y crud
     'agregar_alumno', 'eliminar_alumno', 'leer_alumno', 'actualizar_alumno',
-    'agregar_nota', 'leer_nota', 'actualizar_nota','agregar_nueva_materia', 'eliminar_materia',
+    'agregar_nota', 'leer_nota', 'actualizar_nota','agregar_nueva_materia', 'eliminar_materia', 'asignarmateria',
 
     
     #funciones de login
