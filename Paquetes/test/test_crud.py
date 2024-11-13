fe10cad12e50c4b080f77b1cb0747c28db983e35
mbramuglia <mbramuglia@uade.edu.ar>
2024-11-13 16:48:43 -0300

    Actualizo Test

--- Paquetes/test/test_crud.py
+++ Paquetes/test/test_crud.py
@@ -3,11 +3,9 @@ from Paquetes import validar_mail, validar_fecha_nacimiento #1er test
 
 def  test_LoginMailT(): #testeo del login x mail
     login_pass = validar_mail('mailgenerico@gmail.com')
-    if login_pass == True:
-        assert True
+    assert login_pass == True
 
 
 def test_LoginFecha ():
     login_pass = validar_fecha_nacimiento('11/10/2020')
-    if login_pass == True:
-        assert True
+    assert login_pass == True
